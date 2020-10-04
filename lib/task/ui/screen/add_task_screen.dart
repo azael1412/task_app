54c4743b0973d78c263c5e9a59f8b3af68a66292
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-04 13:57:40 -0500

    se implemeto las pantallas para crear, editar y listar

--- lib/task/ui/screen/add_task_screen.dart
+++ lib/task/ui/screen/add_task_screen.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:task_app/helpers/database_helper.dart';
-import 'package:task_app/task/models/task_model.dart';
+import 'package:task_app/task/models/message.dart';
+
+import 'package:task_app/task/models/task.dart';
+import 'package:task_app/task/bloc/task_bloc.dart';
 import 'package:intl/intl.dart';
 
 class AddTaskScreen extends StatefulWidget {
-  final Function updateTaskList;
   final Task task;
-  AddTaskScreen({Key key, this.task, this.updateTaskList}) : super(key: key);
+  AddTaskScreen({Key key, this.task}) : super(key: key);
 
   @override
   _AddTaskScreenState createState() => _AddTaskScreenState();
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
-  final _formKey = GlobalKey<FormState>();
+  TaskBloc _taskBloc = TaskBloc();
   String _title;
   String _priority;
   DateTime _dateTime = DateTime.now();
+
   TextEditingController _dateController = TextEditingController();
+  TextEditingController _titleController = TextEditingController();
+
   final List<String> _priorities = ['Low', 'Medium', 'High'];
 
-  final DateFormat _dateFormatter = DateFormat('MMMM dd, yyyy');
+  final DateFormat _dateFormatter = DateFormat('MMMM dd, yyyy', 'es_Es');
+  //errors variable
+  String _titleError; //= 'Please enter a task title';
+  String _priorityError; //= 'Please select a priority level';
+  String _dateTimeError; //= 'Please enter a task date';
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
     if (widget.task != null) {
       _title = widget.task.title;
-      _dateTime = widget.task.date;
+      _dateTime = DateTime.parse(widget.task.date);
       _priority = widget.task.priority;
     }
     _dateController.text = _dateFormatter.format(_dateTime);
+    _titleController.text = _title;
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
-    super.dispose();
     _dateController.dispose();
+    _titleController.dispose();
+    super.dispose();
   }
 
   void _handleDatePicker() async {
     final DateTime date = await showDatePicker(
-        context: context,
-        initialDate: _dateTime,
-        firstDate: DateTime(1970),
-        lastDate: DateTime(2100));
+      context: context,
+      initialDate: _dateTime,
+      firstDate: DateTime(1970),
+      lastDate: DateTime(2100),
+      locale: Locale('es', 'ES'),
+    );
     if (date != null && date != _dateTime) {
       setState(() {
         _dateTime = date;
@@ -56,93 +67,118 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
     }
   }
 
-  _submit() {
-    if (_formKey.currentState.validate()) {
-      _formKey.currentState.save();
-      print('$_title, $_dateTime, $_priority');
+  _submit() async {
+    Task task = Task(
+        title: _title,
+        date: "$_dateTime".substring(0, 10),
+        priority: _priority);
 
-      Task task = Task(title: _title, date: _dateTime, priority: _priority);
-      if (widget.task == null) {
-        // Insert the task to our user's database
-        task.status = 0;
-        DatabaseHelper.instance.insertTask(task);
-      } else {
-        // Update the task
-        task.id = widget.task.id;
-        task.status = widget.task.status;
-        DatabaseHelper.instance.updateTask(task);
-      }
-      widget.updateTaskList();
-      Navigator.pop(context);
+    if (widget.task == null) {
+      _taskBloc.create(task).then((response) {
+        if (response is Message) {
+          Navigator.pop(context, [true, response.message]);
+        }
+        if (response is TaskError) {
+          setState(() {
+            _titleError = response.title != null ? response.title[0] : null;
+            _dateTimeError = response.date != null ? response.date[0] : null;
+            _priorityError =
+                response.priority != null ? response.priority[0] : null;
+          });
+        }
+      });
+    } else {
+      task.id = widget.task.id;
+      _taskBloc.update(task).then((response) {
+        if (response is Message) {
+          Navigator.pop(context, response.message);
+        }
+        if (response is TaskError) {
+          setState(() {
+            _titleError = response.title != null ? response.title[0] : null;
+            _dateTimeError = response.date != null ? response.date[0] : null;
+            _priorityError =
+                response.priority != null ? response.priority[0] : null;
+          });
+        }
+      });
     }
   }
+  // }
 
   _delete() {
-    DatabaseHelper.instance.deleteTask(widget.task.id);
+    print("delete");
+    /*DatabaseHelper.instance.deleteTask(widget.task.id);
     widget.updateTaskList();
-    Navigator.pop(context);
+    Navigator.pop(context);*/
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Icon(Icons.arrow_back_ios,
-                      size: 30.0, color: Theme.of(context).primaryColor),
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                  widget.task == null ? 'Add Task' : 'Update Task',
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10.0),
-                Form(
-                    key: _formKey,
+    return WillPopScope(
+      onWillPop: () {
+        _validationBack(context: context);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => _validationBack(
+                        context: context), //Navigator.pop(context),
+                    child: Icon(Icons.arrow_back_ios,
+                        size: 30.0, color: Theme.of(context).primaryColor),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    widget.task == null ? 'Add Task' : 'Update Task',
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
                     child: Column(
                       children: <Widget>[
                         Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20.0),
-                          child: TextFormField(
-                            style: TextStyle(fontSize: 18.0),
-                            decoration: InputDecoration(
-                                labelText: 'Title',
-                                labelStyle: TextStyle(fontSize: 18.0),
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10.0))),
-                            validator: (input) => input.trim().isEmpty
-                                ? 'Please enter a task title'
-                                : null,
-                            onSaved: (input) => this._title = input,
-                            initialValue: this._title,
-                          ),
-                        ),
+                            padding: EdgeInsets.symmetric(vertical: 20.0),
+                            child: TextField(
+                              autofocus: false,
+                              keyboardType: TextInputType.name,
+                              controller: _titleController,
+                              onChanged: (value) => this._title = value,
+                              style: TextStyle(fontSize: 18.0),
+                              decoration: InputDecoration(
+                                  labelText: 'Title',
+                                  errorText: _titleError,
+                                  labelStyle: TextStyle(fontSize: 18.0),
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10.0))),
+                            )),
                         Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20.0),
-                          child: TextFormField(
-                            readOnly: true,
-                            style: TextStyle(fontSize: 18.0),
-                            controller: _dateController,
-                            onTap: _handleDatePicker,
-                            decoration: InputDecoration(
-                                labelText: 'Date',
-                                labelStyle: TextStyle(fontSize: 18.0),
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10.0))),
-                            autofocus: false,
-                          ),
-                        ),
+                            padding: EdgeInsets.symmetric(vertical: 20.0),
+                            child: TextField(
+                              readOnly: true,
+                              style: TextStyle(fontSize: 18.0),
+                              controller: _dateController,
+                              onTap: _handleDatePicker,
+                              decoration: InputDecoration(
+                                  labelText: 'Date',
+                                  errorText: _dateTimeError,
+                                  labelStyle: TextStyle(fontSize: 18.0),
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10.0))),
+                              autofocus: false,
+                            )),
                         Padding(
                           padding: EdgeInsets.symmetric(vertical: 20.0),
                           child: DropdownButtonFormField(
@@ -166,14 +202,13 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                             style: TextStyle(fontSize: 18.0),
                             decoration: InputDecoration(
                               labelText: 'Priority',
+                              errorText: _priorityError,
                               labelStyle: TextStyle(fontSize: 18.0),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0),
                               ),
                             ),
-                            validator: (input) => this._priority == null
-                                ? 'Please select a priority level'
-                                : null,
+                            validator: (input) => this._priorityError,
                             onChanged: (value) {
                               setState(() {
                                 this._priority = value;
@@ -222,12 +257,42 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                               )
                             : SizedBox.shrink(),
                       ],
-                    ))
-              ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _validationBack({BuildContext context}) async {
+    if (_titleController.text.length > 0) {
+      final bool result = await showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (_) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              content: Text("¿Esta seguro de salir?"),
+              actions: [
+                FlatButton(
+                    child: Text("No"),
+                    onPressed: () => Navigator.of(context).pop(false)),
+                FlatButton(
+                    child: Text("Si"),
+                    onPressed: () => Navigator.of(context).pop(true))
+              ],
+            );
+          });
+      if (result) {
+        Navigator.of(context).pop();
+      }
+    } else {
+      Navigator.of(context).pop();
+    }
   }
 }
--- lib/task/ui/screen/list_task_screen.dart
+++ lib/task/ui/screen/list_task_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:task_app/helpers/database_helper.dart';
-import 'package:task_app/task/models/task_model.dart';
-import 'package:task_app/task/ui/screen/add_task_screen.dart';
 
-import 'package:intl/intl.dart';
+import 'package:task_app/task/bloc/task_bloc.dart';
+import 'package:task_app/task/models/message.dart';
+import 'package:task_app/task/models/task.dart';
+
+import 'package:task_app/routes/app_routes.dart';
+import 'package:task_app/widgets/loading_indicator.dart';
+
+import 'package:provider/provider.dart';
+import 'package:task_app/widgets/snackbar_message.dart';
+
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'package:task_app/widgets/fade_transition_route.dart';
+// import 'package:intl/intl.dart';
 
 class ListTaskScreen extends StatefulWidget {
   ListTaskScreen({Key key}) : super(key: key);
@@ -13,126 +22,205 @@ class ListTaskScreen extends StatefulWidget {
 }
 
 class _ListTaskScreenState extends State<ListTaskScreen> {
-  Future<List<Task>> _taskList;
-  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  TaskBloc taskBloc;
+  bool loading = false;
+  ScrollController _scrollController = ScrollController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   void initState() {
     // TODO: implement initState
-    _updateTaskList();
+    super.initState();
+
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //   _updateTaskList(context: context);
+    //   print("entre1");
+    // });
   }
 
-  _updateTaskList() {
-    setState(() {
-      _taskList = DatabaseHelper.instance.getTaskList();
-    });
+  _updateTaskList({BuildContext context}) {
+    taskBloc = Provider.of<TaskBloc>(context, listen: false);
+    taskBloc.getTasks();
   }
 
   @override
   Widget build(BuildContext context) {
-    _taskList = DatabaseHelper.instance.getTaskList();
+    _updateTaskList(context: context);
+    // taskBloc = Provider.of<TaskBloc>(context);
+    // taskBloc.getTasks();
+
     return Scaffold(
-      body: FutureBuilder(
-          future: _taskList,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text("Lista de Tareas"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<List<Task>>(
+          initialData: [],
+          stream: taskBloc.tasks, //taskBloc.tasksStream,
+          builder: (context, AsyncSnapshot<List<Task>> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Loading.loadingIndicators(context: context);
+              case ConnectionState.waiting:
+                return Loading.loadingIndicators(context: context);
+              case ConnectionState.active:
+                print("active");
+                return _listTask(snapshot: snapshot);
+              // return Loading.loadingIndicators(context: context);
+              // case ConnectionState.done:
+              //   print("done");
+              //   return _listTask(snapshot: snapshot);
             }
-            final int completedTaskCount = snapshot.data
-                .where((Task task) => task.status == 1)
-                .toList()
-                .length;
-            return ListView.builder(
-              padding: EdgeInsets.symmetric(vertical: 80.0),
-              itemCount: 1 + snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text('My Tasks',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 40.0,
-                                fontWeight: FontWeight.bold)),
-                        SizedBox(height: 10.0),
-                        Text(
-                          '$completedTaskCount of ${snapshot.data.length}',
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w600),
-                        )
-                      ],
-                    ),
-                  );
-                }
-                return _buidTask(task: snapshot.data[index - 1]);
-              },
-            );
+            if (snapshot.hasError) {
+              return Text("${snapshot.error.toString()}");
+            }
           }),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
-        onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => AddTaskScreen(
-                      updateTaskList: _updateTaskList,
-                    ))),
+        onPressed: () => Navigator.pushNamed(context, AppRoutes.addTask
+                //FadeTransitionRoute(widget: AddTaskScreen())
+                // MaterialPageRoute(
+                //     builder: (context) {
+                //       return AddTaskScreen();
+                //     },
+                //     settings: RouteSettings(name: "AddTaskScreen"))
+                )
+            .then((value) {
+          if (value is List && value != null) {
+            if (value[0] != null && value[0]) {
+              setState(() {
+                loading = true;
+              });
+              _updateTaskList(context: context);
+              SnackBarMessage.message(
+                  text: value[1], scaffoldKey: _scaffoldKey);
+              loading = false;
+            }
+          }
+        }),
         child: Icon(Icons.add),
       ),
     );
   }
 
-  Widget _buidTask({Task task}) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(
-              task.title,
-              style: TextStyle(
-                  fontSize: 18.0,
-                  decoration: task.status == 0
-                      ? TextDecoration.none
-                      : TextDecoration.lineThrough),
-            ),
-            subtitle: Text(
-                '${_dateFormatter.format(task.date)} - ${task.priority}',
-                style: TextStyle(
-                    fontSize: 15.0,
-                    decoration: task.status == 0
-                        ? TextDecoration.none
-                        : TextDecoration.lineThrough)),
-            trailing: Checkbox(
-              onChanged: (value) {
-                task.status = value ? 1 : 0;
-                DatabaseHelper.instance.updateTask(task);
-                _updateTaskList();
-              },
-              activeColor: Theme.of(context).primaryColor,
-              value: task.status == 1 ? true : false,
-            ),
-            onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => AddTaskScreen(
-                        task: task, updateTaskList: _updateTaskList))),
-          ),
-          Divider(),
-        ],
+  Widget _buidTask({Task task, BuildContext context}) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
+      ),
+      onDismissed: (direction) {
+        taskBloc.delete(task).then((value) {
+          if (value is Message) {
+            SnackBarMessage.message(
+                text: value.message, scaffoldKey: _scaffoldKey);
+          }
+        });
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+                title: Text(
+                  "${task.title}",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      decoration: task.status == 0
+                          ? TextDecoration.none
+                          : TextDecoration.lineThrough),
+                ),
+                subtitle: Text('${task.date} - ${task.priority}',
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        decoration: task.status == 0
+                            ? TextDecoration.none
+                            : TextDecoration.lineThrough)),
+                trailing: Checkbox(
+                  onChanged: (value) {
+                    task.status = value ? 1 : 0;
+                    taskBloc.changeStatus(task).then((response) {
+                      if (response is Message) {
+                        _updateTaskList(context: context);
+                        SnackBarMessage.message(
+                            text: response.message, scaffoldKey: _scaffoldKey);
+                      }
+                    });
+                  },
+                  activeColor: Theme.of(context).primaryColor,
+                  value: task.status == 1 ? true : false,
+                ),
+                onTap: () => Navigator.pushNamed(context, AppRoutes.addTask,
+                            arguments: task
+
+                            // FadeTransitionRoute(widget: AddTaskScreen(task: task))
+                            // MaterialPageRoute(
+                            //     builder: (context) {
+                            //       return AddTaskScreen(task: task);
+                            //     },
+                            //     settings: RouteSettings(name: "AddTaskScreen"))
+                            )
+                        .then((value) {
+                      if (value != null) {
+                        setState(() {
+                          loading = true;
+                        });
+                        _updateTaskList(context: context);
+                        print("creando tarea");
+                        SnackBarMessage.message(
+                            text: value, scaffoldKey: _scaffoldKey);
+                        loading = true;
+                      }
+                    })),
+            Divider(),
+          ],
+        ),
       ),
     );
-    /* Container(
-      margin: EdgeInsets.all(10.0),
-      height: 100.0,
-      width: double.infinity,
-      color: Colors.red,
-    );*/
+  }
+
+  Widget _listTask({AsyncSnapshot<List<Task>> snapshot}) {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        print("actual ${_scrollController.position.pixels}");
+        print("max: ${_scrollController.position.maxScrollExtent}");
+        // taskBloc.getTasks();
+      }
+    });
+    int completedTaskCount = 0;
+    completedTaskCount =
+        snapshot.data.where((Task task) => task.status == 1).toList().length;
+    return ListView.builder(
+        physics: BouncingScrollPhysics(),
+        controller: _scrollController,
+        itemCount: snapshot.data.length + 1,
+        itemBuilder: (_, index) {
+          if (index == 0) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('My Tasks',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 10.0),
+                  Text(
+                    '$completedTaskCount of ${snapshot.data.length}',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return _buidTask(task: snapshot.data[index - 1], context: context);
+          }
+        });
   }
 }
