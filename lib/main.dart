68d77690101dad8f77137699e3b432bc8350d256
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-04 13:57:04 -0500

    se separo la clase my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:task_app/task/ui/screen/list_task_screen.dart';
+import 'package:task_app/my_app.dart';
 
+// import 'package:task_app/HomePage.dart';
 //2 building our add task screen
 //minuto 13:09
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Task App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ListTaskScreen(),
-    );
-  }
 }
