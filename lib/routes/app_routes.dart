2921cb9969a40860012e1240d70ba06142f664a0
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-04 13:55:18 -0500

    se separo las rutas del app

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String splashscreen = '/splashScreen';
+  // static const String home = '/home';
+  static const String tasks = '/tasks';
+  static const String addTask = '/addTask';
+}
---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:task_app/routes/app_routes.dart';
+import 'package:task_app/animated/fade_transition_route.dart';
+
+import 'package:task_app/task/ui/screen/add_task_screen.dart';
+import 'package:task_app/task/ui/screen/list_task_screen.dart';
+import 'package:task_app/screens/splash_screen.dart';
+
+class RouteGenerartor {
+  static Route<dynamic> onGenerateRoute({RouteSettings settings}) {
+    switch (settings.name) {
+      case (AppRoutes.splashscreen):
+        return MaterialPageRoute(builder: (context) => SplashScreen());
+
+      case (AppRoutes.tasks):
+        return MaterialPageRoute(builder: (context) => ListTaskScreen());
+
+      case (AppRoutes.addTask):
+        final task = settings.arguments;
+        return FadeTransitionRoute(widget: AddTaskScreen(task: task));
+    }
+  }
+
+  static Route<dynamic> onUnknownRoute({RouteSettings settings}) {
+    return _errorRoute();
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("ERROR"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Pantalla No Encontrada"),
+        ),
+      );
+    });
+  }
+}
