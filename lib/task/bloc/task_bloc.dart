36b53149f6a7c5f9a3dd8dd2e4251fc389cc0e00
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-04 19:06:34 -0500

    se quito el codigo que se iba utilizar para la paginacion

--- lib/task/bloc/task_bloc.dart
+++ lib/task/bloc/task_bloc.dart
@@ -1,38 +1,29 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:task_app/task/models/task.dart';
 import 'package:task_app/task/resources/task_repository.dart';
 
 class TaskBloc extends ChangeNotifier {
+  TaskRepository _repository = TaskRepository();
+
   StreamController<List<Task>> _tasks$ =
       StreamController<List<Task>>.broadcast();
-  // List<Task> _tasks = new List();
-  // Function(List<Task>) get tasksSink => _tasks$.sink.add;
-  // Stream<List<Task>> get tasksStream => _tasks$.stream;
-
-  TaskRepository repository = TaskRepository();
 
   Stream<List<Task>> get tasks => _tasks$.stream;
 
-  Future<dynamic> create(Task task) => repository.create(task);
+  Future<dynamic> create(Task task) => _repository.create(task);
 
-  Future<dynamic> update(Task task) => repository.update(task);
+  Future<dynamic> update(Task task) => _repository.update(task);
 
-  Future<dynamic> changeStatus(Task task) => repository.changeStatus(task);
+  Future<dynamic> changeStatus(Task task) => _repository.changeStatus(task);
 
-  Future<dynamic> delete(Task task) => repository.delete(task);
+  Future<dynamic> delete(Task task) => _repository.delete(task);
 
   Future<List<Task>> getTasks() async {
-    final tasks = await repository.tasks();
+    final tasks = await _repository.tasks();
     _tasks$.sink.add(tasks);
-    // if (tasks != null) {
-    // _tasks$.sink.add(tasks);
-    // tasksSink(tasks);
-    //   _tasks.addAll(tasks);
-    //   _tasks$.sink.add(_tasks);
-    // }
 
-    return tasks != null ? tasks : [];
+    return tasks;
   }
 
   dispose() {
