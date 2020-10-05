35a25cbb8632473f8835b430632860a03e02ab19
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-04 19:04:53 -0500

    se agrego animacion el mensaje de texto del snack

--- lib/widgets/snackbar_message.dart
+++ lib/widgets/snackbar_message.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class SnackBarMessage {
   static void message(
       {@required String text, @required GlobalKey<ScaffoldState> scaffoldKey}) {
     final snackbar = new SnackBar(
-      content: Text(text),
+      content: FadeInRightBig(child: Text(text)),
       duration: Duration(seconds: 3),
     );
     scaffoldKey.currentState.showSnackBar(snackbar);
