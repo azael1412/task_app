63a94cf2a102bca69bf2b73c38ff126b4395deb9
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-11 18:44:19 -0500

    se modifico la forma de enviar parametros y se agrego la obtencion de prioridades

--- lib/task/resources/task_provider.dart
+++ lib/task/resources/task_provider.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
-import 'package:task_app/task/models/message.dart';
+
 import 'package:task_app/task/models/task.dart';
+import 'package:task_app/task/models/message.dart';
+import 'package:task_app/task/models/priority.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -10,10 +12,12 @@ class TaskProvider {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
   };
-  String url = 'http://192.168.0.10:8000/api/task';
+  String authority = '192.168.0.4:8000';
+  String unencodedPath = '/api/task';
 
   Future<List<Task>> tasks() async {
     try {
+      final url = Uri.http(authority, unencodedPath);
       final response = await http.get(url, headers: headers);
       if (response.statusCode == 200) {
         return taskFromJson(response.body);
@@ -27,6 +31,7 @@ class TaskProvider {
   }
 
   Future<dynamic> create(Task task) async {
+    final url = Uri.http(authority, unencodedPath);
     try {
       final response =
           await http.post(url, headers: headers, body: taskToJson(task));
@@ -47,9 +52,10 @@ class TaskProvider {
   }
 
   Future<dynamic> update(Task task) async {
+    final url = Uri.http(authority, unencodedPath + "/${task.id}");
     try {
-      final response = await http.put(url + "/${task.id}",
-          headers: headers, body: taskToJson(task));
+      final response =
+          await http.put(url, headers: headers, body: taskToJson(task));
       switch (response.statusCode) {
         case 200:
           return messageFromJson(response.body);
@@ -71,9 +77,11 @@ class TaskProvider {
   }
 
   Future<dynamic> changeStatus(Task task) async {
+    final url =
+        Uri.http(authority, unencodedPath + "/change-status/${task.id}");
     try {
-      final response = await http.put(url + "/change-status/${task.id}",
-          headers: headers, body: taskToJson(task));
+      final response =
+          await http.put(url, headers: headers, body: taskToJson(task));
       switch (response.statusCode) {
         case 200:
           return messageFromJson(response.body);
@@ -92,8 +100,9 @@ class TaskProvider {
   }
 
   Future<dynamic> delete(Task task) async {
+    final url = Uri.http(authority, unencodedPath + "/${task.id}");
     try {
-      final response = await http.delete(url + "/${task.id}", headers: headers);
+      final response = await http.delete(url, headers: headers);
       switch (response.statusCode) {
         case 200:
           return messageFromJson(response.body);
@@ -105,6 +114,21 @@ class TaskProvider {
         default:
           print('Upps!! Algo salio mal');
           break;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<List<Priority>> priorities() async {
+    final url = Uri.http(authority, "/api/priorities");
+    try {
+      final response = await http.get(url, headers: headers);
+      if (response.statusCode == 200) {
+        return priorityFromJson(response.body);
+      } else {
+        // If that call was not successful, throw an error.
+        throw Exception('Failed to load tasks');
       }
     } catch (e) {
       print(e.toString());
