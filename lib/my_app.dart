2727b4a46be22f1d9e6b6519cd81a66fed8f0e32
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-10-11 18:39:57 -0500

    se quito el manejador de estado

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
-
-import 'package:task_app/task/bloc/task_bloc.dart';
 import 'package:task_app/languages/language.dart';
 import 'package:task_app/routes/app_routes.dart';
 import 'package:task_app/routes/route_generator.dart';
 
-import 'package:provider/provider.dart';
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => TaskBloc(),
-        child: MaterialApp(
-          localizationsDelegates: Language.localizationsDelegates,
-          supportedLocales: Language.supportedLocales,
-          title: 'Task App',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          // home: ListTaskScreen(),
-          initialRoute: AppRoutes.splashscreen,
-          onGenerateRoute: (settings) =>
-              RouteGenerartor.onGenerateRoute(settings: settings),
-          onUnknownRoute: (settings) =>
-              RouteGenerartor.onUnknownRoute(settings: settings),
-        ));
+    return MaterialApp(
+      localizationsDelegates: Language.localizationsDelegates,
+      supportedLocales: Language.supportedLocales,
+      title: 'Task App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // home: ListTaskScreen(),
+      initialRoute: AppRoutes.splashscreen,
+      onGenerateRoute: (settings) =>
+          RouteGenerartor.onGenerateRoute(settings: settings),
+      onUnknownRoute: (settings) =>
+          RouteGenerartor.onUnknownRoute(settings: settings),
+      // )
+    );
   }
 }
